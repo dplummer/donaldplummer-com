6547f4c519c41d5da7bcf7a880483c6ba8c25aae
Donald Plummer <donald.plummer@gmail.com>
2013-08-13 18:20:06 -0700

    Display whole contents of article on home

--- config.rb
+++ config.rb
@@ -17,8 +17,8 @@ activate :blog do |blog|
   # blog.day_link = ":year/:month/:day.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  #blog.tag_template = "tag.html"
+  #blog.calendar_template = "calendar.html"
 
   # blog.paginate = true
   # blog.per_page = 10
